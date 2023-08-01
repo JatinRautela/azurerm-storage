a841932b109603e4970f2cfb3a6c3d17d8e7917b
Jatin Rautela <curioussnake02@gmail.com>
2023-08-01 13:23:31 +0530

    added variable description.

--- variables.tf
+++ variables.tf
@@ -149,7 +149,7 @@ variable "custom_domain" {
 }
 
 variable "network_rules_default_action" {
-  description = "This attribute sets the default network access rule for the storage account. It determines the default action that should be taken if no specific rule matches a request. Possible values for this attribute are "Allow" or "Deny". When set to "Allow", all network access is allowed by default, and when set to "Deny", all network access is denied by default."
+  description = "This attribute sets the default network access rule for the storage account."
   type        = string
   default     = "Deny"
 }
